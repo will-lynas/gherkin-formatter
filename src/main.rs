bb35183cc416e9214c70991f19b410473c5fe046
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-06-29 00:59:21 +0100

    Add basic_feature_bad test
    This tests that a badly indented file gets indented. This obviously
    fails at this point.

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,26 @@ Feature: Guess the word
             "The formatter should leave correct indentation as is."
         );
     }
+
+    #[test]
+    fn basic_feature_bad() {
+        let input = "\
+Feature: Guess the word
+Scenario: Maker starts a game
+When the Maker starts a game
+Then the Maker waits for a Breaker to join";
+
+        let expected = "\
+Feature: Guess the word
+  Scenario: Maker starts a game
+    When the Maker starts a game
+    Then the Maker waits for a Breaker to join"
+            .to_string();
+
+        let result = format(input);
+        assert_eq!(
+            result, expected,
+            "The formatter should indent the feature correctly."
         );
     }
 }
