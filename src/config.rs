0301b1555e5cc9799fcb313847e1eb5cd9d79015
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-06-29 12:36:05 +0100

    Add TrailingNewlineOption

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,18 @@
+pub enum TrailingNewlineOption {
+    Add,
+    NoChange,
+}
+
 pub struct FormatterConfig {
     pub indent_size: usize,
+    pub add_trailing_newline: TrailingNewlineOption,
 }
 
 impl Default for FormatterConfig {
     fn default() -> Self {
-        FormatterConfig { indent_size: 2 }
+        FormatterConfig {
+            indent_size: 2,
+            add_trailing_newline: TrailingNewlineOption::Add,
+        }
     }
 }
