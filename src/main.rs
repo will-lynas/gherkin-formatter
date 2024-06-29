9c51cc28021f24ddac89c2d88922c6f1cb3a4062
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-06-29 11:18:28 +0100

    Make the formatting test pass naively
    Indentation of lines is hardcoded based on what the line starts with

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Read, Write};
 
+const INDENT_SIZE: usize = 2;
+
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
@@ -9,7 +11,22 @@ fn main() -> io::Result<()> {
 }
 
 fn format(input: &str) -> String {
-    input.to_string()
+    let mut result = String::new();
+    for line in input.lines() {
+        if line.starts_with("Feature:") {
+            result.push_str(line);
+        } else if line.starts_with("Scenario:") {
+            result.push_str(&" ".repeat(INDENT_SIZE));
+            result.push_str(line);
+        } else if line.starts_with("When") || line.starts_with("Then") {
+            result.push_str(&" ".repeat(INDENT_SIZE * 2));
+            result.push_str(line);
+        } else {
+            result.push_str(line);
+        }
+        result.push('\n');
+    }
+    result
 }
 
 #[cfg(test)]
