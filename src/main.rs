0825d44fcc2044510bed6bf0265b29950fc24cf7
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-06-29 11:01:44 +0100

    Move tests into own module

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,4 @@ fn format(input: &str) -> String {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn basic_feature_good() {
-        let input = "\
-Feature: Guess the word
-  Scenario: Maker starts a game
-    When the Maker starts a game
-    Then the Maker waits for a Breaker to join";
-
-        let expected = input.to_string();
-        let result = format(input);
-        assert_eq!(
-            result, expected,
-            "The formatter should leave correct indentation as is."
-        );
-    }
-
-    #[test]
-    fn basic_feature_bad() {
-        let input = "\
-Feature: Guess the word
-Scenario: Maker starts a game
-When the Maker starts a game
-Then the Maker waits for a Breaker to join";
-
-        let expected = "\
-Feature: Guess the word
-  Scenario: Maker starts a game
-    When the Maker starts a game
-    Then the Maker waits for a Breaker to join"
-            .to_string();
-
-        let result = format(input);
-        assert_eq!(
-            result, expected,
-            "The formatter should indent the feature correctly."
-        );
-    }
-}
+mod tests;
